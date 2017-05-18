cde3bcf7a0ae4a7b78e39e20bbf36312d1a395de
José Benavides <jbenavides.bermudez@gmail.com>
2017-05-18 15:44:22 -0500

    Routing basics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from 'app/home/welcome.component';
+import { PageNotFoundComponent } from 'app/page-not-found.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: 'welcome', component: WelcomeComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
--- src/app/home/welcome.component.ts
+++ src/app/home/welcome.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
